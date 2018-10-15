3b9993cad3b549fb1654b691c4537a586548dbb4
Martin Kayser <makayser@microsoft.com>
2018-10-15 15:49:50 +0200

    minor updates

--- {{ cookiecutter.repo_name }}/code/data/make_dataset.py
+++ {{ cookiecutter.repo_name }}/code/data/make_dataset.py
@@ -2,8 +2,7 @@
 import click
 import logging
 from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
-
+import configparser
 
 @click.command()
 @click.argument('input_filepath', type=click.Path(exists=True))
@@ -22,9 +21,3 @@ if __name__ == '__main__':
 
     # not used in this stub but often useful for finding various files
     project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
